922c72b1c68b9c7d9d3a31148a45937d3f213138
Abhishek Kumar <abhishek.kumar.1115@gmail.com>
2015-09-02 23:09:34 -0700

    implement parser functionality

--- scripts/parseEspnTeamList.py
+++ scripts/parseEspnTeamList.py
@@ -9,3 +9,23 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.text, "html.parser")
 tables = soup.find_all('ul', class_='medium-logos')
+
+teams = []
+abbreviations = []
+urls = []
+
+for table in tables:
+    teamList = table.find_all('li')
+    for li in teamList:
+        # extract team urls
+        info = li.h5.a
+        # isolate team name from urls
+        teams.append(info.text)
+        url = info['href']
+        urls.append(url)
+        abbreviations.append(url.split('/')[-2])
+
+columns = {'abbreviations': abbreviations}
+teams = pd.DataFrame(columns, index=teams)
+teams.index.name = 'team'
+print(teams)
